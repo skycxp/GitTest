a02b9f8b2d211894e3be07fec892ca9696548dea
skycxp <heavencxp@163.com>
2016-07-09 17:22:11 +0800

    asda

--- src/com/atguigu/git/TestGit.java
+++ src/com/atguigu/git/TestGit.java
@@ -3,7 +3,8 @@ package com.atguigu.git;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
-		System.out.println("这是不是乱码？？");
+		System.out.println("这是不是乱码？？");   
+		System.out.println("asdasd");
 		System.out.println("asdasd");
 	}
 }
